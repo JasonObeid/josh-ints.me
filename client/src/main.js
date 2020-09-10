d836284d3c0d0647609872993c6c555c985b66b3
Jason <jason.obeid98@gmail.com>
2020-09-10 18:40:56 -0400

    fix for patch 10.18

--- client/src/main.js
+++ client/src/main.js
@@ -5,6 +5,7 @@ import router from './router';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'fs';
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
