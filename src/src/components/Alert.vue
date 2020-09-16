31dd8ea0890af1772e5ec871320e1e5c0db559ae
Jason <jason.obeid98@gmail.com>
2020-09-15 20:41:10 -0400

    clean repo and auto-hide alert element

--- src/src/components/Alert.vue
+++ src/src/components/Alert.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-    <b-alert variant="success" show>{{ message }}</b-alert>
-    <br>
+    <b-alert variant="success" v-show="elementVisible">{{ message }}</b-alert>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      elementVisible: true,
+    };
+  },
   props: ['message'],
+  created() {
+    // eslint-disable-next-line no-return-assign
+    setTimeout(() => this.elementVisible = false, 2000);
+  },
 };
+
 </script>
