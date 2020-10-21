865279dc9d024e43f6d3efba5cf791c6db56f647
Jason <jason.obeid98@gmail.com>
2020-10-20 22:13:03 -0400

    update to 10.21, fix builds refresh bug

--- src/api/summoners.py
+++ src/api/summoners.py
@@ -360,6 +360,8 @@ def refreshSummoners():
                     'history': history, 'matchInfo': matchInfo,
                     'startIndex': startIndex, 'endIndex': endIndex, 'rank': rank}
         newSummoners.append(summoner)
+    print('I refreshed')
+    print(newSummoners)
     return newSummoners
 
 
@@ -413,7 +415,7 @@ def getItems2(itemIds):
 
 def getSkills2(order, customSkills):
     skills = []
-    skillMap = {'Q': 1, 'W': 2, 'E': 3}
+    skillMap = {'Q': 1, 'W': 2, 'E': 3, 'R': 4}
     for skill in order:
         skillIndex = skillMap[skill]
         name = customSkills[skillIndex]['name']
